3ffa86bf1ce96e7c5d41227faebb01ea7173d3d2
Mohammed <mohammedredha908@gmail.com>
2024-06-16 00:45:41 +0300

    nvim added highlight on yank

--- nvim/.config/nvim/lua/mohammed/core/options.lua
+++ nvim/.config/nvim/lua/mohammed/core/options.lua
@@ -35,3 +35,11 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+	end,
+})

66217c30b39a62ab4755eb620754ad053c4eb0a0
neckbeard-01000101 <mohammedredha908@gmail.com>
2024-08-29 23:28:41 +0300

    removed some neovim plugins

--- nvim/.config/nvim/lua/mohammed/plugins/color-cycle.lua
+++ nvim/.config/nvim/lua/mohammed/plugins/color-cycle.lua
@@ -1,4 +1,5 @@
 return {
+	-- converts between color formats (hex, rgb, hsl...etc)
 	"NTBBloodbath/color-converter.nvim",
 	config = function()
 		vim.keymap.set("n", "<leader>cc", "<Plug>ColorConvertCycle")
--- nvim/.config/nvim/lua/mohammed/plugins/vimbegood.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"ThePrimeagen/vim-be-good",
-	config = function()
-		vim.keymap.set("n", "<leader>bg", ":VimBeGood<CR>")
-	end,
-}

a42f7f059bdd8caa3d56fa4b7fc5e5a9a0fad529
neckbeard-01000101 <mohammedredha908@gmail.com>
2024-11-01 17:55:45 +0300

    lsp fix

--- nvim/.config/nvim/lua/mohammed/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/mohammed/plugins/colorscheme.lua
@@ -1,42 +1,57 @@
 return {
-	{ "neanias/everforest-nvim" },
-	{ "projekt0n/caret.nvim" },
-	{
-		"catppuccin/nvim",
-		config = function()
-			require("catppuccin").setup({
-				transparent_background = false,
-			})
-			vim.cmd("colorscheme catppuccin")
-		end,
-	},
-	{ "ellisonleao/gruvbox.nvim" },
-	{ "folke/tokyonight.nvim" },
-	{ "cpea2506/one_monokai.nvim" },
-	{
-		"loctvl842/monokai-pro.nvim",
-		config = function()
-			require("monokai-pro").setup({
-				filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
-			})
-		end,
-	},
-	{
-		"rose-pine/neovim",
-		config = function()
-			require("rose-pine").setup({
-				variant = "moon", -- auto, main, moon, or dawn
-				dark_variant = "moon", -- main, moon, or dawn
-				dim_inactive_windows = false,
-				extend_background_behind_borders = true,
-				styels = {
-					transparency = true,
-				},
-			})
-		end,
-	},
-	{ "AlexvZyl/nordic.nvim" },
-	{ "HoNamDuong/hybrid.nvim" },
-	{ "kaiuri/nvim-juliana" },
-	{ "rebelot/kanagawa.nvim" },
+    { "neanias/everforest-nvim" },
+    { "projekt0n/caret.nvim" },
+    {
+        "catppuccin/nvim",
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = false,
+            })
+            vim.cmd("colorscheme kanagawa")
+        end,
+    },
+    { "ellisonleao/gruvbox.nvim" },
+    { "folke/tokyonight.nvim" },
+    { "cpea2506/one_monokai.nvim" },
+    {
+        "loctvl842/monokai-pro.nvim",
+        config = function()
+            require("monokai-pro").setup({
+                filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
+            })
+        end,
+    },
+    {
+        "rose-pine/neovim",
+        config = function()
+            require("rose-pine").setup({
+                variant = "moon", -- auto, main, moon, or dawn
+                dark_variant = "moon", -- main, moon, or dawn
+                dim_inactive_windows = false,
+                extend_background_behind_borders = true,
+                styels = {
+                    transparency = true,
+                },
+            })
+        end,
+    },
+    { "AlexvZyl/nordic.nvim" },
+    { "HoNamDuong/hybrid.nvim" },
+    { "kaiuri/nvim-juliana" },
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require("kanagawa").setup({
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = "none",
+                            },
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }

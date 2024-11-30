0806e16b3b7f2cee43c0ae59e790dac09deb3c80
neckbeard-01000101 <mohammedredha908@gmail.com>
2024-11-30 12:44:19 +0300

    enhanced the installation script

--- install.sh
+++ install.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
 
-# NOTE: this script only works for arch linux
+# NOTE: this script only works for Arch Linux
 
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
-    exit
+    exit 1
 fi
 
 directories=$(find . -maxdepth 1 -type d -not -path '.' -exec basename {} \;)
 
 echo "Installing stow"
-pacman -S --noconfirm stow
+sudo pacman -S --noconfirm stow
 
 for dir in $directories; do
-    echo "Stowing directory: $dir"
-    stow $dir
+    if [ -d "$dir" ]; then
+        echo "Stowing directory: $dir"
+        stow $dir
+    fi
 done
 
-echo "Stowing complete. Now installing packages."
+echo "Stowing complete. Now installing packages..."
 
 for dir in $directories; do
-    echo "Installing package: $dir"
-    pacman -S --noconfirm $dir
+    if [ -d "$dir" ]; then
+        echo "Installing package: $dir"
+        sudo pacman -S --noconfirm $dir
+    fi
 done
 
 echo "Installation complete."
 
+
+echo "Installing extra packages ..."
+sudo pacman -S --noconfirm git swaybg ttf-jetbrains-mono
+
+echo "Installing yay..."
+sudo pacman -S --needed --noconfirm base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --noconfirm
+cd ..
+
+echo "Installing extra AUR packages..."
+yay -S --noconfirm waypaper sway-screenshot brave-bin
+
+echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing zsh-syntax-highlighting..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+echo "Installing zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+source ~/.zshrc
+
+echo "Setup complete."

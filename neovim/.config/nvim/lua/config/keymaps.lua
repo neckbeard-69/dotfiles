3b5bcac06ee89961c2ee396dcbf0d04d93427c9e
mohammed <mohammedredha908@gmail.com>
2025-04-13 08:20:36 +0300

    refined the custom motions (were written by a noob, aka me)

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,51 +1,20 @@
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>nh", ":nohl<CR>")
 
-keymap.set("n", "<leader>sv", "<C-w>v")
-keymap.set("n", "<leader>sh", "<C-w>s")
-keymap.set("n", "<leader>sx", "<cmd>close<CR>")
--- panes navigation
-keymap.set("n", "<A-h>", ":wincmd h<CR>")
-keymap.set("n", "<M-j>", function()
-	if vim.opt.diff:get() then
-		vim.cmd([[normal! ]c]])
-		return
-	end
-	vim.cmd([[m .+1<CR>==]])
-end)
-
-keymap.set("n", "<A-k>", function()
-	if vim.opt.diff:get() then
-		vim.cmd([[normal! [c]])
-		return
-	end
-	vim.cmd([[m .-2<CR>==]])
-end)
-keymap.set("n", "<A-l>", ":wincmd l<CR>")
-keymap.set("n", "<leader>a", "<C-a>") -- doesn't work with tmux
-keymap.set("n", "<leader>x", "<C-x>") -- doesn't work with tmux
+keymap.set("n", "<leader>a", "<C-a>") --the original doesn't work with tmux
+keymap.set("n", "<leader>x", "<C-x>") --the original doesn't work with tmux
 keymap.set("i", "<A-h>", "<Esc>:wincmd h<CR>")
 keymap.set("i", "<A-k>", "<Esc>:wincmd k<CR>")
 keymap.set("i", "<A-j>", "<Esc>:wincmd j<CR>")
 keymap.set("i", "<A-l>", "<Esc>:wincmd l<CR>")
 -- Custom motions
 keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
-keymap.set("n", "y<C-j>", "yG", { desc = "Copy to the end of the file" })
-keymap.set({ "n", "v" }, "<C-j>", "G", { desc = "Go to the end of the file" })
-keymap.set("n", "d<C-j>", "dG", { desc = "Delete to the end of the file" })
-
-keymap.set({ "n", "v" }, "<C-k>", "gg", { desc = "Go to the top of the file" })
-keymap.set("n", "y<C-k>", "ygg", { desc = "Copy to the top of the file" })
-keymap.set("n", "d<C-k>", "dgg", { desc = "Delete to the top of the file" })
-
-keymap.set({ "n", "v" }, "<C-l>", "$", { desc = "Go to the end of the current line" })
-keymap.set("n", "y<C-l>", "y$", { desc = "Copy to the end of the current line" })
-keymap.set("n", "d<C-l>", "d$", { desc = "Delete to the end of the current line" })
-
-keymap.set({ "n", "v" }, "<C-h>", "0", { desc = "Go to the start of the current line" })
-keymap.set("n", "y<C-h>", "y0", { desc = "Copy to the start of the current line" })
-keymap.set("n", "d<C-h>", "d0", { desc = "Delete to the start of the current line" })
+keymap.set({ "n", "x", "o" }, "<C-j>", "G")
+keymap.set({ "n", "x", "o" }, "<C-k>", "gg")
+keymap.set({ "n", "x", "o" }, "<C-l>", "$")
+keymap.set({ "n", "x", "o" }, "<C-h>", "0")
+keymap.set({ "n", "x", "o" }, "m", "%")
 keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
 
 keymap.set("n", "<C-s>", ":w<CR>")
@@ -54,9 +23,9 @@ keymap.set("i", "<C-s>", "<Esc>:w<CR>a")
 keymap.set({ "n", "i", "v" }, "<C-z>", "")
 keymap.set({ "v" }, "p", "P")
 keymap.set("n", "<leader>th", function()
-	require("nvchad.themes").open()
+    require("nvchad.themes").open()
 end, { desc = "telescope nvchad themes" })
 
 vim.keymap.set({ "n", "t" }, "<leader>tt", function()
-	require("nvchad.term").toggle({ pos = "float", id = "floatTerm" })
+    require("nvchad.term").toggle({ pos = "float", id = "floatTerm" })
 end)

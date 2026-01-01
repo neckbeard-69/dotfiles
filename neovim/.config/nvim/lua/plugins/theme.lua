f44990bade80cbb355465dd1918ee6ac1d8d7dfd
mohammed <mohammedredha908@gmail.com>
2026-01-01 21:05:17 +0300

    changed some stuff

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,8 @@
 return {
-    "ellisonleao/gruvbox.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd("colorscheme gruvbox")
+        vim.cmd("colorscheme tokyonight-moon")
     end,
 }
